8aff5d5088cd67ca27a83752a42f997093dcea33
VioletGminisJ25 <javierrodal25@gmail.com>
2024-02-21 09:58:40 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ function start(){
 	document.getElementById("intro").setAttribute("class","hiddenintro");
 	document.getElementById("header").removeAttribute("class", "mostrar");
 	document.getElementById("game").removeAttribute("class", "mostrar");
+	document.getElementById("footer").removeAttribute("class","hidden");
 }
